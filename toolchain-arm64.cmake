b0e652f8029729422735735a199e48a3a1f7dc0b
裙下孤魂 <46797568+zhangbo8418@users.noreply.github.com>
2024-09-15 08:33:23 +0800

    Update toolchain-arm64.cmake

--- toolchain-arm64.cmake
+++ toolchain-arm64.cmake
@@ -6,7 +6,7 @@ set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 set(CMAKE_RANLIB aarch64-linux-gnu-ranlib)
 set(CMAKE_AR aarch64-linux-gnu-ar)
 
-set(CMAKE_FIND_ROOT_PATH /usr/bin /usr/lib/jellyfin-ffmpeg /usr/aarch64-linux-gnu)
+set(CMAKE_FIND_ROOT_PATH /usr/bin /var/packages/Jellyfin/target /usr/aarch64-linux-gnu)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
